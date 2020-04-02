45ed429509e42cccd2b405472af45c32617f2328
Neelabh Madan <neelabh.madan@outlook.com>
2020-04-02 22:53:36 +0530

    pickle file editions

--- test_widerface.py
+++ test_widerface.py
@@ -151,92 +151,23 @@ if __name__ == '__main__':
         scale1 = scale1.to(device)
         landms = landms * scale1 / resize
         landms = landms.cpu().numpy()
+        print(landms.shape)
+        imgResultDict={"conf":scores,"landms":landms,"loc":boxes}
+        # adding it to the main results pickle file
 
-        imgResultDict={"conf":scores,""}
-        #----------------------------------removing this section--------------------------
-        # # ignore low scores
-        # inds = np.where(scores > args.confidence_threshold)[0]
-        # boxes = boxes[inds]
-        # landms = landms[inds]
-        # scores = scores[inds]
-
-        # # keep top-K before NMS
-        # order = scores.argsort()[::-1]
-        # # order = scores.argsort()[::-1][:args.top_k]
-        # boxes = boxes[order]
-        # landms = landms[order]
-        # scores = scores[order]
-
-        # # do NMS
-        # dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
-        # keep = py_cpu_nms(dets, args.nms_threshold)
-        # # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
-        # dets = dets[keep, :]
-        # landms = landms[keep]
-
-        # # keep top-K faster NMS
-        # # dets = dets[:args.keep_top_k, :]
-        # # landms = landms[:args.keep_top_k, :]
-
-        # dets = np.concatenate((dets, landms), axis=1)
+        testResults[img_name]=imgResultDict        
         _t['misc'].toc()
-
-        # --------------------------------------------------------------------
-        #old version
-        # save_name = args.save_folder + img_name[:-4] + ".txt"
-
-        #new version
-        save_name = args.save_folder+args.trained_model.strip(".pth").strip("/weights/")+"/" + img_name[:-4] + ".txt"
-        dirname = os.path.dirname(save_name)
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        with open(save_name, "w") as fd:
-            bboxs = dets
-            file_name = os.path.basename(save_name)[:-4] + "\n"
-            bboxs_num = str(len(bboxs)) + "\n"
-            fd.write(file_name)
-            fd.write(bboxs_num)
-            for box in bboxs:
-                x = int(box[0])
-                y = int(box[1])
-                w = int(box[2]) - int(box[0])
-                h = int(box[3]) - int(box[1])
-                confidence = str(box[4])
-                line = str(x) + " " + str(y) + " " + str(w) + " " + str(h) + " " + confidence + " \n"
-                fd.write(line)
-
         print('im_detect: {:d}/{:d} forward_pass_time: {:.4f}s misc: {:.4f}s'.format(i + 1, num_images, _t['forward_pass'].average_time, _t['misc'].average_time))
-
-        # save image
-        if args.save_image:
-            for b in dets:
-                if b[4] < args.vis_thres:
-                    continue
-                text = "{:.4f}".format(b[4])
-                b = list(map(int, b))
-                cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
-                cx = b[0]
-                cy = b[1] + 12
-                cv2.putText(img_raw, text, (cx, cy),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
-
-                # landms
-                cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
-                cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
-                cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
-                cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
-                cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
-            # save image
-            imgPath=args.save_folder+args.trained_model.strip(".pth").strip("/weights/")+"/imgResults/"
-            if not os.path.exists(imgPath):
-                os.makedirs(imgPath)
-            name = imgPath + str(i) + ".jpg"
-            cv2.imwrite(name, img_raw)
-    a=open("./widerface_evaluate/lastModelRun.txt","w")
-    a.write(args.trained_model[10:-4])
-    a.close()
+    #now saving the pickle file
 
 
+    save_name = args.save_folder+args.trained_model.strip(".pth").strip("/weights/")+"/outResults.pickle"
+    dirname = os.path.dirname(save_name)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    dataFile=open(save_name,"rb")
+    pickle.dump(testResults,dataFile)
+    dataFile.close()
     #now calling evaluation simultaneously
     os.chdir("./widerface_evaluate/")
     str="./widerface_txt/"+args.trained_model.strip(".pth").strip("/weights/")+"/"
