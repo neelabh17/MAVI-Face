b19d274a1ed5bd33eae0282684f06f8eda9aad77
Neelabh Madan <40853567+neelabh17@users.noreply.github.com>
2020-04-30 11:50:02 +0530

    corrected file saving

--- detect.py
+++ detect.py
@@ -82,40 +82,12 @@ if __name__ == '__main__':
     net = net.to(device)
 
     resize = 1
-    # ok=(0,0)
-    # i=600
-    # notFound=True
-    # gfn=True
-    # while(notFound and i<800):
-
-    #     j=600
-    #     if(not gfn):
-    #         gfn=True
-    #     while(notFound and j<800 and gfn):
-
-    #         priorbox = PriorBox(cfg, image_size=(i, j))
-    #         priors = priorbox.forward()
-    #         priors = priors.to(device)
-    #         prior_data = priors.data
-    #         print(" This is the prior box shape",prior_data.shape[0],i,j)
-
-    #         if(prior_data.shape[0]==16800):
-    #             notFound=False
-    #             ok=(i,j)    
-    #         if(prior_data.shape[0]>16800):
-    #             gfn=False
-    #         j+=1
-    #     i+=1
-
-    # print("the ok value is the following :", ok)
-    # testing begin
     for i in range(1):
 
 
         image_path = "nano.jpg"
         img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
         print("The original image shape is ", img_raw.shape )
-        img_raw=cv2.resize(img_raw,(633,633))
         img = np.float32(img_raw)
         im_height, im_width, _ = img.shape
         print("The image shape is ", img.shape )
@@ -126,25 +98,9 @@ if __name__ == '__main__':
         img = img.to(device)
         scale = scale.to(device)
 
-
-        import pickle
-        model_name="faceDetector"
-        dbfile = open(model_name, 'rb')      
-        db = pickle.load(dbfile) 
-        loc2,conf2,landms2=db["conf"],db["loc"],db["landmarks"]
-        loc2=torch.Tensor(loc2).to(device)
-        conf2=torch.Tensor(conf2).to(device)
-        landms2=torch.Tensor(landms2).to(device)
-        ok=6500
-        # loc2=loc2[:,ok:8064+ok,:]
-        # conf2=conf2[:,ok:8064+ok,:]
-        # landms2=landms2[:,ok:8064+ok,:]
-
         tic = time.time()
         loc, conf, landms = net(img)  # forward pass
 
-
-        print(loc2.shape,conf2.shape,landms2.shape)
 
         print('net forward time: {:.4f}'.format(time.time() - tic))
 
@@ -154,21 +110,13 @@ if __name__ == '__main__':
         prior_data = priors.data
         print(" This is the prior box shape",prior_data.shape)
 
-        boxes2 = decode(loc2.data.squeeze(0), prior_data, cfg['variance'])
-        boxes2 = boxes2 * scale / resize
-        boxes2 = boxes2.cpu().numpy()
-        scores2 = conf2.squeeze(0).data.cpu().numpy()[:, 1]
-        print(max(scores2))
-        input()
+        
 
         boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
         boxes = boxes * scale / resize
         boxes = boxes.cpu().numpy()
         scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
-        print(max(scores))
-        input()
 
-        landms2 = decode_landm(landms2.data.squeeze(0), prior_data, cfg['variance'])
         landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
 
         # print(landms)
@@ -176,55 +124,10 @@ if __name__ == '__main__':
                                img.shape[3], img.shape[2], img.shape[3], img.shape[2],
                                img.shape[3], img.shape[2]])
         scale1 = scale1.to(device)
-        landms2 = landms2 * scale1 / resize
-        landms2 = landms2.cpu().numpy()
+        
 
         landms = landms * scale1 / resize
         landms = landms.cpu().numpy()
-
-        # i=0
-        # for b in arrlandms:
-        #         if(i%10==0):
-        #             cv2.circle(img_raw, (b[0], b[1]), 1, (0, 0, 255), 4)
-        #             cv2.circle(img_raw, (b[2], b[3]), 1, (0, 255, 255), 4)
-        #             cv2.circle(img_raw, (b[4], b[5]), 1, (255, 0, 255), 4)
-        #             cv2.circle(img_raw, (b[6], b[7]), 1, (0, 255, 0), 4)
-        #             cv2.circle(img_raw, (b[8], b[9]), 1, (255, 0, 0), 4)
-        #         i+=1
-
-        print(landms2.shape)
-        # ignore low scores
-        inds2 = np.where(scores2 > args.confidence_threshold)[0]
-        boxes2 = boxes2[inds2]
-        landms2 = landms2[inds2]
-        scores2 = scores2[inds2]
-        print(landms2.shape)
-
-        # keep top-K before NMS
-        order2 = scores2.argsort()[::-1][:args.top_k]
-        boxes2 = boxes2[order2]
-        landms2 = landms2[order2]
-        scores2 = scores2[order2]
-        print(landms2.shape)
-
-        # do NMS
-        dets2 = np.hstack((boxes2, scores2[:, np.newaxis])).astype(np.float32, copy=False)
-        keep2 = py_cpu_nms(dets2, args.nms_threshold)
-        print(landms2.shape)
-        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
-        dets2= dets2[keep2, :]
-        landms2 = landms2[keep2]
-        print(landms2.shape)
-
-        # keep top-K faster NMS
-        dets2 = dets2[:args.keep_top_k, :]
-        landms2 = landms2[:args.keep_top_k, :]
-        print(landms2.shape)
-
-        dets2 = np.concatenate((dets2, landms2), axis=1)
-        # print(dets2.shape)
-        
-        
         
         # ignore low scores
         inds = np.where(scores > args.confidence_threshold)[0]
@@ -250,10 +153,10 @@ if __name__ == '__main__':
         landms = landms[:args.keep_top_k, :]
 
         dets = np.concatenate((dets, landms), axis=1)
-        # img_raw=cv2.resize(img_raw,(383,512))
+
         # show image
         if args.save_image:
-            for b in dets2:
+            for b in dets1:
                 if b[4] < args.vis_thres:
                     continue
                 text = "{:.4f}".format(b[4])
