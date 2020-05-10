172d23e3a423857b828addaa7350563625a1590d
Neelabh Madan <neelabh.madan@outlook.com>
2020-05-10 17:44:50 +0530

    Cleaning up- trainer cleaned

--- ok.py
+++
@@ -1,4 +0,0 @@
-import cv2
-a=cv2.imread("a1.jpg")
-from google.colab.patches import cv2_imshow
-cv2_imshow(a)
--- train.py
+++ train.py
@@ -22,6 +22,7 @@ parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='i
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
 parser.add_argument('--resume_net', default='./weights/Resnet50_Final.pth', help='resume net for retraining')
 parser.add_argument('--resume_epoch', default=0, type=int, help='resume iter for retraining')
+parser.add_argument('--save_epoch', default=2, type=int, help='after how many epoche steps should the model be saved')
 parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
 parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
 parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
@@ -55,7 +56,7 @@ training_dataset = args.training_dataset
 validation_dataset = args.validation_dataset
 save_folder = args.save_folder
 
-ohem_location = './data/widerface/ohem/label.txt'
+ohem_dataset = './data/widerface/ohem/label.txt'
 
 net = RetinaFace(cfg=cfg)
 
@@ -82,12 +83,11 @@ cudnn.benchmark = True
 for params in net.parameters():  # set all layers requires_grad to false
     params.requires_grad = False
 
-net.ClassHead = net._make_class_head(fpn_num=3, inchannels=cfg['out_channel'])  # re-initiliaze the layers
-net.BboxHead = net._make_bbox_head(fpn_num=5, inchannels=cfg['out_channel'])
+# re initialising our layers
+net.ClassHead = net._make_class_head(fpn_num=3, inchannels=cfg['out_channel'])  
+# we can think of redcing this fpn from 5 to 3 to increase inference time by a bit
+net.BboxHead = net._make_bbox_head(fpn_num=3, inchannels=cfg['out_channel']) 
 
-# for name, param in net.named_parameters():  # util to print layers that are now trainable
-#     if param.requires_grad:
-#         print (name)
 
 Plist = []
 
@@ -125,8 +125,8 @@ def train():
     dataset_ = data.DataLoader(val_data,batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate)
 
     print('Loading OHEM data...')
-    ohem_data_ = WiderFaceDetection(ohem_location,preproc(img_dim, rgb_mean))
-    ohem_data = data.DataLoader(ohem_data_,batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate)
+    ohem_data = WiderFaceDetection(ohem_dataset,preproc(img_dim, rgb_mean))
+    ohem_data_ = data.DataLoader(ohem_data,batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate)
 
     epoch_size = math.ceil(len(dataset) / batch_size)
     max_iter = max_epoch * epoch_size
@@ -142,20 +142,19 @@ def train():
     epoch_loss_train = 0.0
     lossCollector=[]
 
-    lossrn=2000#just a random value greater than 155 to move on with the first epoch
     for iteration in range(start_iter, max_iter):
         if iteration % epoch_size == 0:
+            # code called for each epoch at begin of the epoch
             # create batch iterator
             batch_iterator = iter(data.DataLoader(dataset, batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate))
-            if (epoch % 2 == 0 and epoch > 0) or (epoch % 5 == 0 and epoch > cfg['decay1']):
+            if (epoch % 2 == 0 and epoch > 0) :
                 torch.save(net.state_dict(), save_folder + cfg['name']+ '_epoch_' + str(epoch) + '_noGrad_FT_Adam_WC1.pth')
             epoch += 1
 
         load_t0 = time.time()
         if iteration in stepvalues:
             step_index += 1
-        lr = adjust_learning_rate_by_neels(optimizer,lossrn)
-
+        lr = adjust_learning_rate(optimizer, gamma, epoch, step_index, iteration, epoch_size)
         # load train data
         images, targets = next(batch_iterator)
         images = images.cuda()
@@ -180,15 +179,16 @@ def train():
 
         # calculate validation loss after each epoch
         if iteration % epoch_size == 0:
+            # run for each epoch at then end of each epoch
+
             # print('Training loss per image for Epoch {} : {}'.format(epoch,epoch_loss_train/len(dataset)))
             print('Training loss for Epoch {} : {}'.format(epoch,epoch_loss_train))
 
             # calc val loss after each epoch
             valLoss=train_eval(net,dataset_,batch_size,epoch)
-            lossrn=valLoss
 
             # also calc ohem_loss after each epoch
-            ohem_loss = train_eval(net,ohem_data,batch_size,epoch,is_ohem=True)
+            ohem_loss = train_eval(net,ohem_data_,batch_size,epoch,is_ohem=True)
 
             lossCollector.append({"Epoch":epoch,"TrainLoss":epoch_loss_train,"ValLoss":valLoss,"OhemLoss":ohem_loss})
             
@@ -209,18 +209,6 @@ def train():
 
     picklefile.close()
 
-def adjust_learning_rate_by_neels(optimizer,lossrn):
-    """Sets the learning rate
-    # Adapted from PyTorch Imagenet example:
-    # https://github.com/pytorch/examples/blob/master/imagenet/main.py
-    """
-    if(lossrn>155):
-        lr=1e-3
-    else:
-        lr=1e-4
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-    return lr
 def adjust_learning_rate(optimizer, gamma, epoch, step_index, iteration, epoch_size):
     """Sets the learning rate
     # Adapted from PyTorch Imagenet example:
@@ -228,8 +216,10 @@ def adjust_learning_rate(optimizer, gamma, epoch, step_index, iteration, epoch_s
     """
     warmup_epoch = -1
     if epoch <= warmup_epoch:
+        #basically this isnt going to run like ever
         lr = 1e-6 + (initial_lr-1e-6) * iteration / (epoch_size * warmup_epoch)
     else:
+        #we are going to run this one
         lr = initial_lr * (gamma ** (step_index))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
