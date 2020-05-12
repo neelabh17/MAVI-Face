47269831d820be75a51c7e3e25c13c8b39a583b1
neelabh17 <neelabh.madan@outlook.com>
2020-05-12 20:09:22 +0000

    clean

--- gitCred.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("git config --global user.email \'neelabh.madan@outlook.com\'")        
-os.system("git config --global user.name \'neelabh17\'")
-print("Credentials Entered")
--- gitToken.py
+++
@@ -1,7 +0,0 @@
-print("Enter new token")
-token=input()
-print("Changing token")
-import os
-myCommand="git remote set-url origin https://{}@github.com/neelabh17/MAVI-Face.git".format(token)
-
-os.system(myCommand)
